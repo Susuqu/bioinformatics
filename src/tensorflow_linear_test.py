da3cbc985e76d047ba21f93c570570dfee1d88a4
Susuqu <qususu2012@126.com>
2018-04-26 17:53:25 +0800

    add a new py file

---
+++ src/tensorflow_linear_test.py
@@ -0,0 +1,9 @@
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib
+from mpl_toolkits.mplot3d import Axes3D
+#tensorflow需要用这个版本，否则报错
+    # pip uninstall tensorflow
+    # pip install tensorflow==1.5
+
