fd5eee002b0b90ffee45bec0fee1527eab995186
Susuqu <qususu2012@126.com>
2018-05-13 14:52:05 +0800

    modify scripts

--- src/2_drug_assess.py
+++ src/2_drug_assess.py
@@ -7,16 +7,10 @@ import matplotlib.pyplot as plt
 '''
 plot可以优化的点：subgroup画图的循环怎么实现？
 缺失值的替换不成功；
-
-
 '''
-
-
 data = pd.read_table("2_drug_arrange.txt", encoding='utf-8',na_values=[" ","-","NONE",'X','#VALUE!','#DIV/0!'])    #为啥文件名是中文的时候就有问题呢？
 # print(data.shape)   #(468,31)
 # print(data['ssattp1'])  #看下空值之类的是否替换成功
-
-
 
 # 分别对以下列（分类变量）绘制bar plot: pharm, sex, subtype
 def drawBar(colname):
@@ -59,18 +53,49 @@ def drawHist(colname,groupnumber):
 # drawHist(colname='hiScoreT',groupnumber=50)   #教师评分用药前后减分——多动冲动分量表分数
 # drawHist(colname='hiRatioT',groupnumber=150)  #教师评分用药前后减分比率——多动冲动分量表分数
 
-
 # adhdpr1, adhdpr, atScoreP, atRatioP, hiScoreP, hiRatioP, adhdtr1, adhdtr, atScoreT, atRatioT, hiScoreT, hiRatioT
 # groupby sex, subtype, pharm
-def drawSubBar(colname,subname,kind):
-    data[[subname, colname]].groupby([colname, subname]).hiRatioP.count().unstack().plot(kind=kind,stacked=True)  # 默认的为line
+
+
+def drawSubBar(colname,subname,kind,a,b,c,d):
+    # data[[subname, colname]].groupby([colname, subname]).hiRatioP.count().unstack().plot(kind=kind,stacked=True)  # 默认的为line
+    plt.subplot2grid((a,b),(c,d),colspan=3)
+    print(a,b,c,d)
+    '''
+    表示将整个图像窗口分成a行b列
+    a=1
+    b=3
+    c,d表示小图在大图中的位置
+    '''
+    data.groupby([colname, subname])[[colname]].count().unstack().plot(kind=kind, stacked=True)
     # print(col_value)
     plt.xlabel('{}'.format(colname))
     plt.ylabel('Frequency')
     plt.title('distribution in all samples')
-    plt.savefig(r'E:\OuMengCompany\Project\ScientificResearchService\ADHD新样本性状分析\fig\drug'+os.sep+'{}_{}_{}.{}'.format(colname,subname,kind,'png'))
+    plt.legend(loc='best')
+    plt.savefig(r'E:\OuMengCompany\Project\ScientificResearchService\ADHD新样本性状分析\fig\drugNew'+os.sep+'{}_{}_{}.{}'.format(colname,subname,kind,'png'))
+    # plt.show()
+    # plt.close()
+
+# colnamelist=['adhdpr1', 'adhdpr', 'atScoreP', 'atRatioP', 'hiScoreP', 'hiRatioP', 'adhdtr1', 'adhdtr', 'atScoreT', 'atRatioT', 'hiScoreT', 'hiRatioT']
+# subnamelist=['sex','pharm','subtype']
+# plotkind=['bar','line']
+# for name in colnamelist:
+#     for sub in subnamelist:
+#         for style in plotkind:
+#             drawSubBar(colname=name,subname=sub,kind=style)
+# colnamelist=['adhdpr1', 'adhdpr', 'atScoreP', 'atRatioP', 'hiScoreP', 'hiRatioP', 'adhdtr1', 'adhdtr', 'atScoreT', 'atRatioT', 'hiScoreT', 'hiRatioT']
+plt.figure()
+colnamelist=['adhdpr1']
+subnamelist=['sex','pharm','subtype']
+row=0
+col=0
+for name in colnamelist:
+    for sub in subnamelist:
+        drawSubBar(colname=name,subname=sub,kind='bar',a=1,b=3,c=row,d=col)
+        col=col+1
     plt.show()
-    plt.close()
+# plt.close()
 
 # drawSubBar(colname='adhdpr1',subname='sex',kind='bar')
 # drawSubBar(colname='adhdpr1',subname='sex',kind='line')
@@ -85,5 +110,20 @@ def drawSubBar(colname,subname,kind):
 # drawSubBar(colname='hiRatioP',subname='subtype',kind='line')
 # drawSubBar(colname='hiRatioT',subname='pharm',kind='line')
 
-# col_value=data[['sex','adhdpr1']].groupby(['adhdpr1','sex']).adhdpr1.count().unstack().plot(kind='bar',stacked=True)    #默认的为line
+# groupby sex, subtype, pharm
+def testdrawSubBar(colname,subname,kind):
+    # data[[subname, colname]].groupby([colname, subname]).count().unstack().plot(kind=kind,stacked=True)  # 默认的为line
+    group_subname=data.groupby([colname, subname])[[colname]].count().unstack()
+    print(group_subname)
+    # data.groupby([colname,subname])[[colname,subname]].count().unstack().plot(kind=kind,stacked=True)#各自分布的叠加
+    data.groupby([colname,subname])[[colname]].count().unstack().plot(kind=kind,stacked=True)  # colname分布为基础，subname中不同类别的分布是否一致
+    plt.xlabel('{}'.format(colname))
+    plt.ylabel('Frequency')
+    plt.title('distribution in all samples')
+    plt.legend(('female','male'),loc='best')    #legend顺序是根据实际注明的
+    plt.savefig(r'E:\OuMengCompany\Project\ScientificResearchService\ADHD新样本性状分析\fig\drug\test'+os.sep+'{}_{}_{}.{}'.format(colname,subname,kind,'png'))
+    plt.show()
+    plt.close()
 
+# testdrawSubBar(colname='adhdpr1',subname='sex',kind='bar')
+# drawSubBar(colname='adhdpr1',subname='sex',kind='line')
