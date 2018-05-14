ce62dff1e459e32d7cec96f20517ed57b27310d7
Susuqu <qususu2012@126.com>
2018-05-14 20:43:35 +0800

    debug & add new codes

--- src/1_score_assess.py
+++ src/1_score_assess.py
@@ -6,14 +6,37 @@ import matplotlib.pyplot as plt
 from pylab import *
 
 '''
-plot可以优化的点：subgroup画图的循环怎么实现？
 缺失值的替换不成功；
-ADHD诊断分组时，为什么有两个ADHD-C好像？是不是数据清洗还是什么过程有问题？
+ADHD诊断分组时，为什么有两个ADHD-C好像？是不是数据清洗还是什么过程有问题？——找到了问题出在哪里：原来是有一个ADHD-C后面多了一个空格，所以被当成了两个类别。
+
 '''
-data = pd.read_table("1_score_arrange.txt", encoding='gbk',na_values=[" ","-","NONE",'X','#VALUE!','#DIV/0!'])
+data = pd.read_table("1_score_arrange.txt", encoding='utf-8',na_values=[" ","-","NONE",'X','#VALUE!'])
 # print(data.shape)   #(282, 10)
+# df.loc[df['columnName']=='the value']
 
 mpl.rcParams['font.sans-serif'] = ['SimHei']    #这一行必须加，否则u的作用就无法实现
+
+control=data.loc[data[u'诊断']=='NC']
+case=data.loc[data[u'诊断']!='NC']
+# print(case)
+
+def drawCaseHist(df,colname,groupnumber,filename):
+    col_value=df[colname].dropna().sort_values().values
+    # print(col_value)
+    plt.hist(col_value,groupnumber)
+    plt.xlabel('{}'.format(colname))
+    plt.ylabel('Frequency')
+    plt.title('distribution in all samples')
+    plt.savefig(r'E:\OuMengCompany\Project\ScientificResearchService\ADHD新样本性状分析\fig\score'+os.sep+'{}_{}.{}'.format(filename,colname,'png'))
+    plt.show()
+    plt.close()
+
+namelist=[u'数字广度粗分',u'数字广度量表分']
+# namelist=[u'月龄',u'正背',u'倒背',u'数字广度粗分',u'数字广度量表分']
+for name in namelist:
+    drawCaseHist(colname=name,groupnumber=15,df=case,filename='case')
+    drawCaseHist(colname=name, groupnumber=15, df=control,filename='control')
+
 
 # 分别对以下列（分类变量）绘制bar plot: 月龄，性别，诊断
 def drawBar(colname):
